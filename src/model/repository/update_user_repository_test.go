11ca27e6079e2c1c5b534659ef817e988ecc5915
matheuszzy <matheuspessoal123@gmai.com.br>
2024-05-05 16:12:49 -0300

    add: test update user

---
+++ src/model/repository/update_user_repository_test.go
@@ -0,0 +1,60 @@
+package repository
+
+import (
+	"os"
+	"testing"
+
+	"github.com/matheuszzy/crud-basico-go/src/model"
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
+)
+
+func TestUpdateUser(t *testing.T) {
+	databaseName := "user_database_test"
+	collectionName := "user_collection_test"
+
+	err := os.Setenv("MONGODB_USER_DB", collectionName)
+	if err != nil {
+		t.FailNow()
+		return
+	}
+	defer os.Clearenv()
+
+	mtestDb := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+
+	mtestDb.Run("when_sending_a_valid_user_returns_success", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 1},
+			{Key: "n", Value: 1},
+			{Key: "acknowledged", Value: true},
+		})
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewUserRepository(databaseMock)
+		domain := model.NewUserDomain(
+			"test@test.com", "test", "test", 90)
+		domain.SetId(primitive.NewObjectID().Hex())
+
+		err := repo.UpdateUser("test", domain)
+
+		assert.Nil(t, err)
+	})
+
+	mtestDb.Run("return_error_from_database", func(mt *mtest.T) {
+		mt.AddMockResponses(bson.D{
+			{Key: "ok", Value: 0},
+		})
+		databaseMock := mt.Client.Database(databaseName)
+
+		repo := NewUserRepository(databaseMock)
+		domain := model.NewUserDomain(
+			"test@test.com", "test", "test", 90)
+		domain.SetId(primitive.NewObjectID().Hex())
+
+		err := repo.UpdateUser(domain.GetID(), domain)
+
+		assert.NotNil(t, err)
+	})
+}
